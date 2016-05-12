d764a5af3f02419f6c748fbd37889bcbc7af1ad5
cartifon <ciceroartifon@gmail.com>
2016-05-11 21:52:10 -0300

    falsy bouncer

---
+++ app.js
@@ -0,0 +1,13 @@
+/*
+function keep(value) {
+    return value;
+}
+
+function bouncer(arr) {
+  return arr.filter(keep);
+}
+
+// console.log(bouncer([7, "ate", "", false, 9]));
+console.log(bouncer([false, null, 0, NaN, undefined, ""]));
+console.log(bouncer([1, null, NaN, 2, undefined]));
+*/
