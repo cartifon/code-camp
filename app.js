d5b3b14e5b7ca2ad861a03351abbfc2473860487
cartifon <ciceroartifon@gmail.com>
2016-05-11 22:46:22 -0300

    Revert "falsy bouncer"
    This reverts commit c726393a8f8851694499454fb0d4c8898219c62d.

--- app.js
+++ app.js
@@ -11,16 +11,3 @@ function bouncer(arr) {
 console.log(bouncer([false, null, 0, NaN, undefined, ""]));
 console.log(bouncer([1, null, NaN, 2, undefined]));
 */
-
-
-function destroyer(arr) {
-    for (var i = 1; i < arguments.length; i++) {
-        if(arr.indexOf(arguments[i]) >= 0) {
-            arr.splice(arr.indexOf(arguments[i]), 1);
-            i--;
-        }
-    }
-    return arr;
-}
-
-console.log(destroyer([1, 2, 3, 1, 2, 3], 2, 3));
